e1f3fda7aa5ef6469e882229c4ff9b2380bceac6
Maxwell Jung <jungmaxwell@gmail.com>
2023-01-19 11:23:24 -0800

    merge labs into single Keil project

--- Keil/RTE/_release/RTE_Components.h
+++ Keil/RTE/_release/RTE_Components.h
@@ -3,8 +3,8 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'lab_2' 
- * Target:  'Target 1' 
+ * Project: 'ece153b_lab' 
+ * Target:  'release' 
  */
 
 #ifndef RTE_COMPONENTS_H
