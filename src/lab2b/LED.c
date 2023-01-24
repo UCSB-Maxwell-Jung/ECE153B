1de730fd8312f0a8a46fe583835f9680d6734adb
Maxwell Jung <jungmaxwell@gmail.com>
2023-01-24 12:19:20 -0800

    copy lab2a LED code

--- src/lab2b/LED.c
+++ src/lab2b/LED.c
@@ -1,29 +1,47 @@
 /*
  * ECE 153B - Winter 2021
  *
- * Name(s):
- * Section:
+ * Name(s): Baron Young, Maxwell Jung
+ * Section: Wed 7pm
  * Lab: 2B
  */
 
 #include "LED.h"
 
-void LED_Init(void) {
-	// Enable GPIO Clocks
-	// [TODO] 
-	
+void LED_Init(void)
+{
+	// Enable GPIO Clock
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+	// GPIO Mode: Input(00), Output (01),
+	// AF(10), Analog (11)
+	GPIOA->MODER &= ~3U << 10; // reset to 00
+	GPIOA->MODER |= 1U << 10;  // set to 01
+
+	// 2.3b Set the output type of PA 5 as Push-Pull
+	GPIOA->OTYPER &= ~1U << 5; // reset to 0
+	GPIOA->OTYPER |= 0u << 5;  // set to 0
+
+	// GPIO Push-Pull: No pull-up, pull-down (00),
+	// Pull-up (01), Pull-down (10), Reserved (11)
+	GPIOA->PUPDR &= ~3U << 10; // reset to 00
+	GPIOA->PUPDR |= 0U << 10;  // set to 00
+
 	// Initialize Green LED
-	// [TODO]
+	Green_LED_Off();
 }
 
-void Green_LED_Off(void) {
-	// [TODO]
+void Green_LED_Off(void)
+{
+	GPIOA->ODR &= ~1U << 5;
 }
 
-void Green_LED_On(void) {
-	// [TODO]
+void Green_LED_On(void)
+{
+	GPIOA->ODR |= 1U << 5;
 }
 
-void Green_LED_Toggle(void){
-	// [TODO]
+void Green_LED_Toggle(void)
+{
+	GPIOA->ODR ^= 1U << 5;
 }
