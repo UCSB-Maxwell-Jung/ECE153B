8a10c2bee38f165387d582aeb9b99ba6554aa50b
Maxwell Jung <jungmaxwell@gmail.com>
2023-01-24 12:43:50 -0800

    fire timer interrupt every 1ms

--- src/lab2b/SysTimer.c
+++ src/lab2b/SysTimer.c
@@ -8,14 +8,14 @@
 
 #include "SysTimer.h"
 
-uint32_t volatile msTicks;
+uint32_t static volatile msTicks;
 
 void SysTick_Init(void) {
 	// SysTick Control & Status Register
 	SysTick->CTRL = 0; // Disable SysTick IRQ and SysTick Counter
 	
 	// SysTick Reload Value Register
-	SysTick->LOAD = 100000; // [TODO] - Enter the correct LOAD value that will give us a 1 ms period
+	SysTick->LOAD = (1*1000000)*0.001-1; // (clock freq * desired period) - 1 = number of ticks needed
 	
 	// SysTick Current Value Register
 	SysTick->VAL = 0;
@@ -27,15 +27,18 @@ void SysTick_Init(void) {
 	// Enables SysTick exception request
 	// 1 = counting down to zero asserts the SysTick exception request
 	// 0 = counting down to zero does not assert the SysTick exception request
-	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
-	
+	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk; // set to 1
+
 	// Select clock source
 	// If CLKSOURCE = 0, the external clock is used. The frequency of SysTick clock is the frequency of the AHB clock divided by 8.
 	// If CLKSOURCE = 1, the processor clock is used.
-	SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk;		
-	
+	SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk; // reset to 0
+
 	// Enable SysTick IRQ and SysTick Timer
-	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;  
+	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+
+	// set the starting tick count as 0
+	msTicks = 0;
 }
 
 void SysTick_Handler(void) {
