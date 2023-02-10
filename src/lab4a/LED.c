0e65e1b5c304e74a9bd0a373746126693b694148
BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>
2023-02-10 09:34:15 -0800

    finished part a termite light enable

--- src/lab4a/LED.c
+++ src/lab4a/LED.c
@@ -1,17 +1,25 @@
 #include "LED.h"
 
-void LED_Init(void) {
-	// [TODO]
+void LED_Init(void)
+{
+	// Enable GPIO Clock
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+	//GPIO mode for green LED
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_1;
 }
 
-void Green_LED_Off(void) {
-	// [TODO]
+void Green_LED_Off(void)
+{
+	GPIOA->ODR &= ~GPIO_ODR_OD5;
 }
 
-void Green_LED_On(void) {
-	// [TODO]
+void Green_LED_On(void)
+{
+	GPIOA->ODR |= GPIO_ODR_OD5;
 }
 
-void Green_LED_Toggle(void) {
-	// [TODO]
+void Green_LED_Toggle(void)
+{
+	GPIOA->ODR ^= GPIO_ODR_OD5;
 }
