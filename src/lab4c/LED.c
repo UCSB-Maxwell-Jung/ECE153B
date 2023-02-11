65555d1df417b314b6d30b1aabdada4f3e89b5bc
Maxwell Jung <jungmaxwell@gmail.com>
2023-02-10 17:50:18 -0800

    initialize LED

--- src/lab4c/LED.c
+++ src/lab4c/LED.c
@@ -8,7 +8,26 @@
 
 #include "LED.h"
 
-void LED_Init(void) {
-	// TODO
+void LED_Init(void)
+{
+	// Enable GPIO Clock
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+	// set to output mode (01)
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_1;
 }
 
+void Green_LED_Off(void)
+{
+	GPIOA->ODR &= ~GPIO_ODR_OD5;
+}
+
+void Green_LED_On(void)
+{
+	GPIOA->ODR |= GPIO_ODR_OD5;
+}
+
+void Green_LED_Toggle(void)
+{
+	GPIOA->ODR ^= GPIO_ODR_OD5;
+}
