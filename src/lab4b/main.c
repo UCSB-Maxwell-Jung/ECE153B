152949ccc3cdd71f3b64b448442a900d61f0543f
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2023-02-16 13:40:40 -0800

    Working lab4b (#67)
    * finished part a termite light enable
    
    * conditions for I2C set, not checked
    
    * correction in calculation comment
    
    * finished code in main and I2C_init, needs testing
    
    * configure PB6, PB7
    
    * init usart
    
    * fix i2c timing
    
    * convert to signed 2's complement
    
    cast to int before subtraction
    
    * setup lab4b on Keil
    
    * minor styling fix
    
    * clear compiler warnings
    
    * temperature sensor datasheet
    
    * working lab4b
    
    ---------
    
    Co-authored-by: BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>

--- src/lab4b/main.c
+++ src/lab4b/main.c
@@ -1,16 +1,26 @@
 /*
  * ECE 153B - Winter 2023
  *
- * Name(s):
- * Section:
+ * Name(s): Baron Young, Maxwell Jung
+ * Section: Wed 7pm
  * Lab: 4B
  */
+#define RTR 0x00
 
 #include "stm32l476xx.h"
 #include "I2C.h"
 #include "SysClock.h"
 #include "UART.h"
 #include <string.h>
+#include <stdio.h>
+
+// Note the "<< 1" must be present because bit 0 is treated as a don't care in 7-bit addressing mode
+static uint8_t SlaveAddress = 0x48 << 1; // A0 sensor (0b1001000);
+static uint8_t Data_Receive;
+static uint8_t command = RTR; // Read Temperature Register
+static int temperature = 0x80; // lowest possible temperature
+
+void Init_USARTx(int x);
 
 // Initializes USARTx
 // USART2: UART Communication with Termite
@@ -30,6 +40,8 @@ void Init_USARTx(int x) {
 }
 
 int main(void) {
+	int i;
+
 	System_Clock_Init(); // System Clock = 80 MHz
 	
 	// Initialize I2C
@@ -38,23 +50,20 @@ int main(void) {
 
 	// Initialize UART -- change the argument depending on the part you are working on
 	Init_USARTx(2);
-	
-	int i;
-	uint8_t SlaveAddress;
-	uint8_t Data_Receive;
-	uint8_t Data_Send;
+	printf("USART initialized\n");
+
 	while(1) {	
-		// Determine Slave Address
-		//
-		// Note the "<< 1" must be present because bit 0 is treated as a don't care in 7-bit addressing mode
-		SlaveAddress = 0b1111111 << 1; // STUB - Fill in correct address 
-		
-		// [TODO] - Get Temperature
-		// 
+		// Get Temperature
 		// First, send a command to the sensor for reading the temperature
+		I2C_SendData(I2C1, SlaveAddress, &command, 1);
 		// Next, get the measurement
-		
-		// [TODO] - Print Temperature to Termite
+		I2C_ReceiveData(I2C1, SlaveAddress, &Data_Receive, 1);
+		// Print Temperature to Termite
+		// convert unsigned 8 bit to 2's complement signed 8 bit
+		// for 8 bit binary, 2's complement is equal to
+		// value of magnitude bits - 2^(n-1) * sign bit
+		temperature = (int)(0x7f & Data_Receive) - (int)(0x80 & Data_Receive);
+		printf("Temperature: %dC\n", temperature);
 		
 		// Some delay
 		for(i = 0; i < 50000; ++i); 
