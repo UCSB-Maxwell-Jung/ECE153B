38e9c08785190cd3a9991438f03b9fd0099090cd
BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>
2023-02-23 15:33:21 -0800

    Lab5b code fully implemented but Untested (#92)
    Changes made to: DAC.c, EXTI.c, main.c, and PWM.c
    DAC.c: implemented DAC_Init and DAC_Write_Value
    EXTI.c: implemented EXTI_Init (used code from lab2) and added handler from interrupt (increments or decrements depending on direction)
    PWM.c: configures port A pin 5 for PWM

--- src/lab5b/EXTI.c
+++ src/lab5b/EXTI.c
@@ -1,9 +1,24 @@
 #include "EXTI.h"
 
 #include "DAC.h"
-
+//Step 3.2 Interrupt .1
 void EXTI_Init(void) {
-    // [TODO] Configure EXTI for button
+    //CODE TAKEN FROM LAB2
+  // Connect External Line to the GPIO
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13;
+	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PC;
+
+	// Rising trigger selection
+	// 0 = trigger disabled, 1 = trigger enabled
+	EXTI->FTSR1 |= EXTI_RTSR1_RT13;
+
+	// Interrupt Mask Register
+	// 0 = masked, 1 = not masked (enabled)
+	EXTI->IMR1 |= EXTI_IMR1_IM13;
+
+	NVIC_EnableIRQ(EXTI15_10_IRQn); // Enable
+	NVIC_SetPriority(EXTI15_10_IRQn, 0); // set interrupt to highest priority
 }
 
 #define DAC_MIN 0
@@ -16,4 +31,33 @@ static enum {
     UP,
 } direction = UP;
 
-// [TODO] Interrupt handler
+//step 3.2 .2
+void EXTI15_10_IRQHandler(void) {
+	// Clear interrupt pending bit
+	if ((EXTI->PR1 & EXTI_PR1_PIF13) != 0) {
+		// PC13 button is default HIGH for some reason
+		dac_value = (DAC->DHR12R1); //read from reg
+        if(direction == UP)
+            if(dac_value < DAC_MAX)
+                dac_value += DAC_INCREMENT;
+            else
+            {
+                 direction = DOWN;
+                 dac_value -= DAC_INCREMENT;
+            }
+        }
+        else
+        {
+            if(dac_value > DAC_MIN)
+                dac_value -= DAC_INCREMENT;
+            else
+            {
+                direction = UP;
+                dac_value += DAC_INCREMENT;
+            }
+        }
+        DAC_Write_Value(dac_value); 
+		// Cleared flag by writing 1
+ 		EXTI->PR1 |= EXTI_PR1_PIF13;
+	}
+}
