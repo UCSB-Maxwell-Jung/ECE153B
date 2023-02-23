4d7eefa85a77b5293b36b966e91c5119d50998ce
BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>
2023-02-23 09:13:43 -0800

    implemented DAC_Pin_Init, as well as EXTI_init, and its handler

--- src/lab5b/DAC.c
+++ src/lab5b/DAC.c
@@ -7,24 +7,34 @@ static void DAC_Pin_Init(void) {
 	GPIOB->MODER |= GPIO_MODER_MODER4; // set to Analog mode (11)
 	
 	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD4; // set to No Pull=Up, No Pull-Down (00)
+
 }
 
 void DAC_Write_Value(uint32_t value) {
     // [TODO] Write DAC1Ch1 right-aligned 12-bit value
+    DAC->DHR12R1 = value;
 }
 
 void DAC_Init(void) {
     DAC_Pin_Init();
+    //Step 3.1
+    // [DONE] Enable DAC clock
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; //.1
 
-    // [TODO] Enable DAC clock
+    DAC->CR &= ~(DAC_CR_EN1); //.2
 
-    // [TODO] Enable software trigger mode
+    // [DONE] Enable software trigger mode
+    DAC->CR &= ~(DAC_CR_TSEL1);
+    DAC->CR |= DAC_CR_TSEL1; //.3
+    
+    // [DONE] Disable trigger
+    DAC->CR &= ~(DAC_CR_TEN1); //.4
 
-    // [TODO] Disable trigger
+    // [DONE] DAC1 connected in normal mode to external pin only with buffer enabled
+    DAC->MCR &= ~(DAC_MCR_MODE1);
 
-    // [TODO] DAC1 connected in normal mode to external pin only with buffer enabled
-
-    // [TODO] Enable DAC channel 1
+    // [DONE] Enable DAC channel 1
+    DAC->CR |= DAC_CR_EN1; //.5
 
     DAC_Write_Value(0);
 }
