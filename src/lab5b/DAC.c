f5d4d7d0c4be133f923b1c16fb9835558e1597f4
Maxwell Jung <jungmaxwell@gmail.com>
2023-02-23 21:15:20 -0800

    configure GPIO A not B

--- src/lab5b/DAC.c
+++ src/lab5b/DAC.c
@@ -4,9 +4,9 @@ static void DAC_Pin_Init(void) {
     // configure PA4 as Analog, No Pull=Up, No Pull-Down
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; // enable GPIOA clock
 	
-	GPIOB->MODER |= GPIO_MODER_MODER4; // set to Analog mode (11)
+	GPIOA->MODER |= GPIO_MODER_MODER4; // set to Analog mode (11)
 	
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD4; // set to No Pull=Up, No Pull-Down (00)
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD4; // set to No Pull=Up, No Pull-Down (00)
 }
 
 // Write DAC1Ch1 right-aligned 12-bit value
