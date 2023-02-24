ee3f8b7c004a59f2dafd390c1faed4d16a39f457
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2023-02-23 23:38:15 -0800

    Working Lab5b (#93)
    * make code compilable
    
    * add updated sysclock
    
    * fix comment for sysclock
    
    * copy adc code from lab5a
    
    * mark pwm_init as done
    
    * fix indentation
    
    * rewrite interrupt logic
    
    * remove unnecessary logic in dac write function
    
    * initialize button
    
    * configure GPIO A not B
    
    * enable the correct DAC clock
    
    * copy pwm code from lab5a
    
    * initialize PWM

---
+++ src/lab5b/Button.h
@@ -0,0 +1,16 @@
+/*
+ * ECE 153B - Winter 2021
+ *
+ * Name(s): Maxwell Jung, Baron Young
+ * Section: Wed 7pm
+ * Lab: 2A
+ */
+
+#ifndef __STM32L476G_NUCLEO_BUTTON_H
+#define __STM32L476G_NUCLEO_BUTTON_H
+
+#include "stm32l476xx.h"
+
+void Button_Init(void);
+
+#endif
