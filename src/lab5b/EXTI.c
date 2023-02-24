7c9601182794eba0a808a05e3fe4ab38ffa4284b
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2023-02-24 11:50:42 -0800

    Finish Lab5b (#82)
    * add lab5b on Keil
    
    * set up PA4
    
    * Lab5b code fully implemented but Untested (#92)
    
    Changes made to: DAC.c, EXTI.c, main.c, and PWM.c
    DAC.c: implemented DAC_Init and DAC_Write_Value
    EXTI.c: implemented EXTI_Init (used code from lab2) and added handler from interrupt (increments or decrements depending on direction)
    PWM.c: configures port A pin 5 for PWM
    
    * Working Lab5b (#93)
    
    * make code compilable
    
    * add updated sysclock
    
    * fix comment for sysclock
    
    * copy adc code from lab5a
    
    * mark pwm_init as done
    
    * fix indentation
    
    * rewrite interrupt logic
    
    * remove unnecessary logic in dac write function
    
    * initialize button
    
    * configure GPIO A not B
    
    * enable the correct DAC clock
    
    * copy pwm code from lab5a
    
    * initialize PWM
    
    * disable lab5a
    
    ---------
    
    Co-authored-by: BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>

--- src/lab5b/EXTI.c
+++ src/lab5b/EXTI.c
@@ -1,19 +1,55 @@
 #include "EXTI.h"
 
 #include "DAC.h"
-
+// Step 3.2 Interrupt .1
+// CODE TAKEN FROM LAB2
 void EXTI_Init(void) {
-    // [TODO] Configure EXTI for button
+    // Connect External Line to the GPIO
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13;
+	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PC;
+
+	// Rising trigger selection
+	// 0 = trigger disabled, 1 = trigger enabled
+	EXTI->FTSR1 |= EXTI_RTSR1_RT13;
+
+	// Interrupt Mask Register
+	// 0 = masked, 1 = not masked (enabled)
+	EXTI->IMR1 |= EXTI_IMR1_IM13;
+
+	NVIC_EnableIRQ(EXTI15_10_IRQn); // Enable
+	NVIC_SetPriority(EXTI15_10_IRQn, 0); // set interrupt to highest priority
 }
 
 #define DAC_MIN 0
 #define DAC_MAX 4095
-#define DAC_INCREMENT 256
+#define DAC_DELTA 256
 
-static uint32_t dac_value = 0;
-static enum {
-    DOWN,
-    UP,
-} direction = UP;
+static int next_dac_value = 0;
+static enum {DOWN, UP} direction = UP;
 
-// [TODO] Interrupt handler
+//step 3.2 .2
+void EXTI15_10_IRQHandler(void) {
+	if ((EXTI->PR1 & EXTI_PR1_PIF13) == EXTI_PR1_PIF13) {
+		next_dac_value = (DAC->DHR12R1); // read from reg
+
+        if (direction == UP) {
+            next_dac_value += DAC_DELTA;
+            if (next_dac_value > DAC_MAX) {
+                next_dac_value = DAC_MAX;
+                direction = DOWN;
+            }
+        }
+        else if (direction == DOWN) {
+            next_dac_value -= DAC_DELTA;
+            if (next_dac_value < DAC_MIN) {
+                next_dac_value = DAC_MIN;
+                direction = UP;
+            }
+        }
+        
+        DAC_Write_Value(next_dac_value); 
+		// Cleared flag by writing 1
+ 		EXTI->PR1 |= EXTI_PR1_PIF13;
+	}
+}
--- src/lab5b/main.c
+++ src/lab5b/main.c
@@ -9,25 +9,40 @@
 
 #include "ADC.h"
 #include "DAC.h"
+#include "Button.h"
 #include "EXTI.h"
 #include "PWM.h"
 #include "SysClock.h"
 #include "stm32l476xx.h"
 #include <stdio.h>
 
+uint32_t data;
+double voltage;
+double vref = 3.3;
+
 int main(void) {
     // Initialization
-    System_Clock_Init(); // Switch System Clock = 16 MHz
+    System_Clock_Init(); // Switch System Clock = 8 MHz
 
     ADC_Init();
     DAC_Init();
+	Button_Init();
     EXTI_Init();
 
-    // [TODO] Initialize PWM
+    // Initialize PWM
+    PWM_Init();
 
     while (1) {
-        // [TODO] Trigger ADC and get result
-
-        // [TODO] LED behavior based on ADC result
+        // Start Regular Conversion
+		ADC1->CR |= ADC_CR_ADSTART;
+		
+		// Wait for ADC conversion
+		while((ADC1->ISR & ADC_ISR_EOC) == 0);
+		
+		// Ready ADC data register
+		data = ADC1->DR;
+		voltage = data/4095.0*vref;
+		
+		set_LED_voltage(voltage, vref); // set led brightness according to vin and vref
     }
 }
