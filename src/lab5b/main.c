959033332598f459fd5e2fd7aed791646b454e92
Maxwell Jung <jungmaxwell@gmail.com>
2023-02-23 20:39:33 -0800

    fix indentation

--- src/lab5b/main.c
+++ src/lab5b/main.c
@@ -31,23 +31,23 @@ int main(void) {
 
     while (1) {
         // Start Regular Conversion
-			ADC1->CR |= ADC_CR_ADSTART;
-			
-			// Wait for ADC conversion
-			while((ADC1->ISR & ADC_ISR_EOC) == 0);
-			
-			// Ready ADC data register
-			data = ADC1->DR;
-			
-			voltage = 3.3*((4096-((float)data))/4096);
-			
-			if (data > 3985) {
-				TIM2->CCR1 = 4096;
-			} else {
-				TIM2->CCR1 &= ~TIM_CCR1_CCR1;
-				TIM2->CCR1 |= data;
-			}
-			
-			for(int i=0; i<1000; ++i); // Some Delay
+		ADC1->CR |= ADC_CR_ADSTART;
+		
+		// Wait for ADC conversion
+		while((ADC1->ISR & ADC_ISR_EOC) == 0);
+		
+		// Ready ADC data register
+		data = ADC1->DR;
+		
+		voltage = 3.3*((4096-((float)data))/4096);
+		
+		if (data > 3985) {
+			TIM2->CCR1 = 4096;
+		} else {
+			TIM2->CCR1 &= ~TIM_CCR1_CCR1;
+			TIM2->CCR1 |= data;
+		}
+		
+		for(int i=0; i<1000; ++i); // Some Delay
     }
 }
