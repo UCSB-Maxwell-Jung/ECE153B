f69e14167fcf352cda970e87f794f208afee9d28
Maxwell Jung <jungmaxwell@gmail.com>
2023-02-28 11:20:46 -0800

    try using c++

---
+++ src/final/EXTI.h
@@ -0,0 +1,25 @@
+/*
+ * ECE 153B - Winter 2021
+ *
+ * Name(s): Maxwell Jung, Baron Young
+ * Section: Wed 7pm
+ * Lab: 2A
+ */
+
+#ifndef __STM32L476G_NUCLEO_EXTI_H
+#define __STM32L476G_NUCLEO_EXTI_H
+
+#include "stm32l476xx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void EXTI_Init(void);
+    void EXTI15_10_IRQHandler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/final/LED.h
@@ -0,0 +1,28 @@
+/*
+ * ECE 153B - Winter 2021
+ *
+ * Name(s): Maxwell Jung, Baron Young
+ * Section: Wed 7pm
+ * Lab: 2A
+ */
+
+#ifndef __STM32L476G_NUCLEO_LED_H
+#define __STM32L476G_NUCLEO_LED_H
+
+#include "stm32l476xx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void LED_Init(void);
+
+    void Green_LED_Off(void);
+    void Green_LED_On(void);
+    void Green_LED_Toggle(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
