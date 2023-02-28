501efe765c6722afcad3fee113dc4aa3f9092a10
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2023-02-28 14:41:31 -0800

    Set up UART for printing to termite terminal (#99)
    * add lcd datasheet
    
    * add Arduino drivers from Adafruit:
    
    https://github.com/adafruit/Adafruit_ILI9341
    https://github.com/adafruit/Adafruit-GFX-Library
    
    * add nucleo driver code:
    
    https://github.com/ardnew/ILI9341-STM32-HAL
    
    * delete github folders
    
    * try using c++
    
    * setup print to terminal
    
    * layout into arduino style code
    
    * working print to termite

---
+++ src/final/UART_printf.c
@@ -0,0 +1,28 @@
+#include "UART.h"
+#include <stdio.h>
+
+// Implement a dummy __FILE struct, which is called with the FILE structure.
+struct _FILE {
+	int dummy;
+};
+
+// We have to define FILE if prinf is used
+FILE __stdout;
+FILE __stdin;
+ 
+// Retarget printf() to USART1/USART2
+int fputc(int ch, FILE *f) { 
+	uint8_t c;
+	c = ch & 0x00FF;
+	USART_Write(USART2, (uint8_t *)&c, 1); // Termite
+	// USART_Write(USART1, (uint8_t *)&c, 1); // Bluetooth
+	return(ch);
+}
+
+// Retarget scanf() to USART1/USART2
+int fgetc(FILE *f) {  
+	uint8_t rxByte;
+	rxByte = USART_Read(USART2); // Termite
+	// rxByte = USART_Read(USART1); // Bluetooth
+	return rxByte;
+}
