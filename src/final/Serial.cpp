93a361089d009047916be43585860552501a09e4
Maxwell Jung <jungmaxwell@gmail.com>
2023-02-28 23:49:51 -0800

    working print to terminal

--- src/final/Serial.cpp
+++ src/final/Serial.cpp
@@ -109,31 +109,3 @@ void Serial::print(char* str) {
 void Serial::println(char* str) {
 	printf("%s\n", str);
 }
-
-uint8_t Serial::read() {
-	// SR_RXNE (Read data register not empty) bit is set by hardware
-	while (!(_USARTx->ISR & USART_ISR_RXNE));  // Wait until RXNE (RX not empty) bit is set
-	// USART resets the RXNE flag automatically after reading DR
-	return ((uint8_t)(_USARTx->RDR & 0xFF));
-	// Reading USART_DR automatically clears the RXNE flag 
-}
-
-void Serial::write(uint8_t *buffer, uint32_t nBytes) {
-	int i;
-	// TXE is cleared by a write to the USART_DR register.
-	// TXE is set by hardware when the content of the TDR 
-	// register has been transferred into the shift register.
-	for (i = 0; i < nBytes; i++) {
-		while (!(_USARTx->ISR & USART_ISR_TXE));   	// wait until TXE (TX empty) bit is set
-		// Writing USART_DR automatically clears the TXE flag 	
-		_USARTx->TDR = buffer[i] & 0xFF;
-		delay(300);
-	}
-	while (!(_USARTx->ISR & USART_ISR_TC));   		  // wait until TC bit is set
-	_USARTx->ISR &= ~USART_ISR_TC;
-}
-
-void Serial::delay(uint32_t us) {
-	uint32_t time = 100*us/7;
-	while(--time);
-}
