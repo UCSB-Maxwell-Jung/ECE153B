782d2da9371814df61c0310b1c4713bc684b7466
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2023-03-06 21:26:41 -0800

    Try camera driver on CubeMX (#117)
    * add manuals
    
    * add camera to Cube IDE

---
+++ Cube/camera/Core/Src/sccb_bus.h
@@ -0,0 +1,42 @@
+
+#ifndef _SCCB_BUS_H_
+#define _SCCB_BUS_H_
+
+
+
+#include "sys.h"
+
+#define I2C_TIM 1
+
+#define SCCB_SIC_H()      PBout(10) =1	 	//SCL H
+#define SCCB_SIC_L()      PBout(10) = 0	 	//SCL H
+#define SCCB_SID_H()      PBout(11) = 1   //SDA	H
+#define SCCB_SID_L()      PBout(11) = 0  //SDA	H 
+
+
+//REFER to Pg 172 for STM32F10x manual
+
+//#define SCCB_DATA_IN     {GPIOB->CRH&=0XFFFF0FFF;GPIOB->CRH|=0X00008000;}//Mode set to 00, CNF = 10
+							//input mode :pull up pull down
+
+#define SCCB_DATA_IN {GPIOB->}
+
+//#define SCCB_DATA_OUT    {GPIOB->CRH&=0XFFFF0FFF;GPIOB->CRH|=0X00003000;} //Mode set to 11, CNF = 00
+							//Out put mode: general purpose push pull
+
+
+//#define SCCB_SID_STATE	  PBin(11)
+#define SCCB_SID_STATE	  PBin()
+
+void sccb_bus_init(void);
+void sccb_bus_start(void);
+void sccb_bus_stop(void);
+void sccb_bus_send_noack(void);
+void sccb_bus_send_ack(void);
+uint8_t sccb_bus_write_byte(uint8_t data);
+uint8_t sccb_bus_read_byte(void);
+
+
+#endif /* _SCCB_BUS_H_ */
+
+/******************* (C) COPYRIGHT 2015 WangBao *****END OF FILE****/
